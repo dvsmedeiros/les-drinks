0ff2c3243bf68146fa8c25d3fc86f150635ae85f
Davisson <dvisson.medeiros@gmail.com>
2017-08-21 23:38:30 -0300

    corrige nome das rotas

--- public/js/main.js
+++ public/js/main.js
@@ -4,25 +4,25 @@ angular.module('lesdrinks', ['ngRoute', 'ngResource', 'appService'])
 		$httpProvider.interceptors.push('tokenInterceptor');
 
 		$routeProvider.when('/drinks', {
-			templateUrl: 'partials/products.html',
+			templateUrl: 'partials/drinks.html',
 			controller: 'DrinksController'
 		});
 
-		$routeProvider.when('/drinks/new', {
+		$routeProvider.when('/drink/new', {
 			templateUrl: 'partials/drink.html',
-			controller: 'DrinksController'
+			controller: 'DrinkController'
 		});
 
-		$routeProvider.when('/drinks/edit/:drinkId', {
+		$routeProvider.when('/drink/edit/:drinkId', {
 			templateUrl: 'partials/drink.html',
-			controller: 'DrinksController'
+			controller: 'DrinkController'
 		});
 
 		$routeProvider.when('/login', {
 			templateUrl: 'partials/login.html',
 			controller: 'LoginController'
 		});
-		
+
 		$routeProvider.otherwise({redirectTo: '/drinks'});
 
 	});
