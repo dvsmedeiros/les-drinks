af477a1df813d757df02a0b67a0f767289e06f9d
Davisson <dvisson.medeiros@gmail.com>
2017-08-21 23:41:20 -0300

    mapeia resources drink, supplier, manufacturer, category

--- public/js/services/app-service.js
+++ public/js/services/app-service.js
@@ -1,48 +1,30 @@
 angular.module('appService', ['ngResource'])
-	
-	/*
-	.factory('recursoFoto', function($resource) {
-
-		return $resource('/v1/fotos/:fotoId', null, {
+	.factory('categoryResource', function($resource) {
+		return $resource('/category/:categoryId', null, {
 			'update' : { 
 				method: 'PUT'
 			}
 		});
 	})
-	.factory("cadastroDeFotos", function(recursoFoto, $q) {
-		var service = {};
-		service.cadastrar = function(foto) {
-			return $q(function(resolve, reject) {
-
-				if(foto._id) {
-					recursoFoto.update({fotoId: foto._id}, foto, function() {
-						resolve({
-							mensagem: 'Foto ' + foto.titulo + ' atualizada com sucesso',
-							inclusao: false
-						});
-					}, function(erro) {
-						console.log(erro);
-						reject({
-							mensagem: 'Não foi possível atualizar a foto ' + foto.titulo
-						});
-					});
-
-				} else {
-					recursoFoto.save(foto, function() {
-						resolve({
-							mensagem: 'Foto ' + foto.titulo + ' incluída com sucesso',
-							inclusao: true
-						});
-					}, function(erro) {
-						console.log(erro);
-						reject({
-							mensagem: 'Não foi possível incluir a foto ' + foto.titulo
-						});
-					});
-				}
-			});
-		};
-		return service;
-    })
-    */
+	.factory('manufacturerResource', function($resource) {
+		return $resource('/manufacturer/:manufacturerId', null, {
+			'update' : { 
+				method: 'PUT'
+			}
+		});
+	})
+	.factory('supplierResource', function($resource) {
+		return $resource('/supplier/:supplierId', null, {
+			'update' : { 
+				method: 'PUT'
+			}
+		});
+	})
+	.factory('drinkResource', function($resource) {
+		return $resource('/drink/:drinkId', null, {
+			'update' : { 
+				method: 'PUT'
+			}
+		});
+	})
     ;
