1122908f413e98f2ef5a04d5c7254b0dc041bef5
Davisson <dvisson.medeiros@gmail.com>
2017-08-23 13:42:43 -0300

    implementa remove

--- public/js/controllers/drinks-controller.js
+++ public/js/controllers/drinks-controller.js
@@ -1,27 +1,42 @@
-angular.module('lesdrinks').controller('DrinksController', function($scope, categoryResource, supplierResource, drinkResource){
+angular.module('lesdrinks').controller('DrinksController', function($scope, $routeParams, categoryResource, supplierResource, drinkResource){
 
 	$scope.message = '';
 	$scope.categories = [];
 	$scope.suppliers = [];
 	$scope.drinks = [];
+	$scope.drinkFilter = {};
 
 
-	categoryResource.query({active: true}, function(categories){
+	categoryResource.query(function(categories){
 		$scope.categories = categories;
 	}, function(error){
 		console.log(error);
 	});
 
-	supplierResource.query({active: true}, function(suppliers){
+	supplierResource.query(function(suppliers){
 		$scope.suppliers = suppliers;
 	}, function(error){
 		console.log(error);
 	});
 
-	drinkResource.query({active: false}, function(drinks){
+	drinkResource.query(function(drinks){
 		$scope.drinks = drinks;
 	}, function(error){
 		console.log(error);
 	});
+
+	$scope.remove = function(drink){
+
+		drinkResource.delete({drinkId: drink._id}, function() {
+			var index = $scope.drinks.indexOf(drink);
+			$scope.drinks.splice(index, 1);
+		}, function(erro) {
+			console.log(erro);
+		});
+	};
+
+	$scope.findByFilter = function(){
+	
+	};
 
 });
