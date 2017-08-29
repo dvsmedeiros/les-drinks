ee708451ff454b9e0c0692602a1950e091aba6ee
Davisson <dvisson.medeiros@gmail.com>
2017-08-29 02:14:16 -0300

    remove resource não utilizado

--- public/js/controllers/resource-controller.js
+++ public/js/controllers/resource-controller.js
@@ -11,19 +11,4 @@ angular.module('lesdrinks').controller('ResourceController', function ($scope) {
 		}
 	];
 
-	$scope.prices = [
-		{
-			id: 1,
-			value: 'R$0.00 à R$100.00'
-		},
-		{
-			id: 2,
-			value: 'R$100.00 à R$200.00'
-		},
-		{
-			id: 3,
-			value: 'R$200.00 ou Mais'
-		}
-	];
-
 });
